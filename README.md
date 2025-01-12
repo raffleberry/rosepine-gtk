d8d16f3b9e30d21b0338068c07c1af9268d6af3d
raffleberry <raffleberry42@gmail.com>
2025-01-12 20:08:27 +0530

    feat: add gtk theme install chore: update docs

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # rosepine-gtk
 A shell script that installs `rosepine` on Gnome desktop.
 Tested on `Ubuntu 22.04`
+
+# install
+1.
+```sh
+git clone --recurse-submodules --depth=1 git@github.com:raffleberry/rosepine-gtk.git
+```
+**backup your gnome-terminal profile before running install**
+```
+./install.sh
+```
--- install.sh
+++ install.sh
@@ -5,16 +5,22 @@ WD=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 dconf load /org/gnome/terminal/legacy/profiles:/ < ./gnome-terminal/dist/rose-pine.dconf
 
-SETTINGS_FILE = ~/.config/gtk-3.0/settings.ini
-if [ -f $SETTINGS_FILE ]; then
-    mv $SETTINGS_FILE "$SETTINGS_FILE.bak"
+THEMES_DIR="$HOME/.themes"
 
-fi
+ICONS_DIR="$HOME/.local/share/icons"
 
-echo "
-[Settings]
-gtk-application-prefer-dark-theme=true
-gtk-theme-name=rose-pine-gtk
-gtk-icon-theme-name=rose-pine-icons
-"
-> $SETTINGS_FILE 
+GTK4_CONFIG_DIR="$HOME/.config/gtk-4.0"
+
+GTK3_DIR="gtk/gtk3"
+GTK4_DIR="gtk/gtk4"
+ROSE_PINE_ICONS="gtk/icons"
+
+mkdir -p "$THEMES_DIR" "$ICONS_DIR" "$GTK4_CONFIG_DIR"
+
+cp -r "$GTK3_DIR"/* "$THEMES_DIR"
+
+cp -r "$GTK4_DIR"/* "$GTK4_CONFIG_DIR"/
+
+cp -r "$ROSE_PINE_ICONS"/* "$ICONS_DIR"
+
+cp -r ./gtk/gnome_shell/moon/gnome-shell "$THEMES_DIR"/rose-pine-moon-gtk/
